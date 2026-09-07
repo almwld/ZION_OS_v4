a9fe092b6ddddddf410361013cfb7c717b4f1db2
almwld <152663389+almwld@users.noreply.github.com>
2026-09-07 03:26:22 +0300

    test: import Flutter UI types explicitly

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zion_os/main.dart';
 
